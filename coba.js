e17ac6470dfbcc4ddb323c0b6848ac252282625a
kikiAgustin <kikiagustin62@gmail.com>
2022-10-08 10:32:47 +0600

    firs commit

---
+++ coba.js
@@ -0,0 +1,5 @@
+function cetakNama(nama) {
+    return `Hallo nama Saya ${nama}`;
+}
+
+module.exports = cetakNama;
---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log('Helo dunia');
+
+// const nama = "Kiki Agustin";
+// const umur = 23;
+// console.log(`Hallo nama saya ${nama}, umur saya adalah ${umur}`);
+
+// const sayHello = (nama, umur) => `Hallo nama saya ${nama} dan umur saya adalah ${umur}`;
+
+const cetakNama = require('./coba');
+
+console.log(cetakNama('Kiki Agustin'));
